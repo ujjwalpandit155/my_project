d160b18fe5655c24e143c0ce113cf4d95c4b55b5
ujjwalpandit155 <109529996+ujjwalpandit155@users.noreply.github.com>
2024-04-30 18:54:15 +0530

    new

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const wrapper = document.querySelector('.wrapper');
  const loginLink = document.querySelector('.login-link');
- const registerLink = document.querySelector('.register-link');
+ const Teachers = document.querySelector('.Teachers');
+ const Students = document.querySelector('.Students');
+//  const registerLink = document.querySelector('.register-link');
  const btnPopup = document.querySelector('.btnLogin-popup');
  const iconClose = document.querySelector('.icon-close');
 
  
 
 
- registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
-});
+//  registerLink.addEventListener('click', ()=> {
+//     wrapper.classList.add('active');
+// });
 
 loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
